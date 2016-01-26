febc6e9e62d8c3e62fbad898297d196af2cdd41e
J/A <archer884@gmail.com>
2016-01-26 13:02:47 -0600

    Mimick foreign method in native rust

--- examples/test.rs
+++ examples/test.rs
@@ -1,6 +1,34 @@
 extern crate cffi;
 
+trait Boolish {
+    fn as_bool(&self) -> bool;
+}
+
+impl Boolish for i32 {
+    fn as_bool(&self) -> bool {
+        *self != 0
+    }
+}
+
+impl Boolish for bool {
+    fn as_bool(&self) -> bool {
+        *self
+    }
+}
+
 fn main() {
-    println!("7 is prime == {}", cffi::is_prime(7));
-    println!("7 is prime == {}", cffi::foreign_is_prime(7));
+    println!("{}", build_result(7, cffi::is_prime));
+    println!("{}", build_result(7, cffi::foreign_is_prime));
+    println!("{}", build_result(7, cffi::is_prime_foreign_clone));
+}
+
+fn build_result<F, R>(candidate: u64, f: F) -> String
+    where R: Boolish,
+          F: FnOnce(u64) -> R,
+{
+    if f(candidate).as_bool() {
+        format!("{} is prime", candidate)
+    } else {
+        format!("{} is not prime", candidate)
+    }
 }
--- src/prime.c
+++ src/prime.c
@@ -1,8 +1,12 @@
 int c_is_prime(unsigned long long int number) {
-    if (number <= 1) return 0; // zero and one are not prime
-    unsigned int i;
+    if (number <= 1)
+        return 0;
+
+    unsigned long long int i;
+
     for (i=2; i*i<=number; i++) {
         if (number % i == 0) return 0;
     }
+
     return 1;
 }
