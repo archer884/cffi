d804f55799bec080d290a11d7fe1c13c6cdbc12e
J/A <archer884@gmail.com>
2016-01-29 16:21:22 -0600

    Test my code

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 extern crate libc;
 extern crate test;
 
+#[cfg(test)]
+extern crate primal;
+
 use libc::{c_int, c_ulonglong};
 
 extern {
@@ -19,7 +22,7 @@ pub fn is_prime(n: u64) -> bool {
     match n {
         0 | 1 => false,
         2 | 3 => true,
-        n if n & 1 == 1 => false,
+        n if n & 1 != 1 => false,
         n => {
             for i in (3..).step_by(2).take_while(|i| i * i <= n) {
                 if n % i == 0 {
@@ -47,6 +50,7 @@ pub fn is_prime_foreign_clone(n: u64) -> bool {
 
 #[cfg(test)]
 mod tests {
+    use primal::Primes;
     use super::{foreign_is_prime, is_prime, is_prime_foreign_clone};
     use test;
 
@@ -69,5 +73,19 @@ mod tests {
         b.iter(|| for n in 1000000..1001000 {
             test::black_box(foreign_is_prime(n));
         })
+    }
+
+    // I believe it's reasonable to ask me to demonstrate that my implementation of this prime
+    // function works, because... Yeah. I don't have that much confidence in myself, honestly. :)
+    #[test]
+    fn native_works() {
+        let first_thousand_primes: Vec<_> = Primes::all().take(1000).map(|n| n as u64).collect();
+        let x = find_first_thousand_primes();
+
+        assert_eq!(&first_thousand_primes, &x);
+    }
+
+    fn find_first_thousand_primes() -> Vec<u64> {
+        (1..).filter(|&n| is_prime(n)).take(1000).collect()
     }
 }
--- src/prime.c
+++ src/prime.c
@@ -2,9 +2,12 @@ int c_is_prime(unsigned long long int number) {
     if (number <= 1)
         return 0;
 
+    if (number == 2)
+        return 0;
+
     unsigned long long int i;
 
-    for (i=2; i*i<=number; i++) {
+    for (i=3; i*i<=number; i+=2) {
         if (number % i == 0) return 0;
     }
 
